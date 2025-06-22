b1dc69e59cb3addc7f179b43e8ee7ca3173bcc4e
netilson29 <kingaristides@gmail.com>
2025-06-22 06:34:16 +0100

    Atualiza requirements.txt e corrige app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 def buscar_noticias():
-    url = 'https://news.google.com/rss/search?q=tecnologia+OR+portugal+OR+angola&hl=pt-PT&gl=PT&ceid=PT:pt-150'
+    url = 'https://news.google.com/rss/search?q=tecnologia&hl=pt-PT&gl=PT&ceid=PT:pt'
     resposta = requests.get(url)
     soup = BeautifulSoup(resposta.content, 'xml')
     itens = soup.find_all('item')[:10]
@@ -20,7 +20,7 @@ def buscar_noticias():
             'titulo': titulo,
             'link': link,
             'resumo': descricao,
-            'imagem': 'https://source.unsplash.com/400x200/?news,tech'  # imagem genérica automática
+            'imagem': 'https://source.unsplash.com/400x200/?technology'
         }
         noticias.append(noticia)
 
@@ -30,6 +30,3 @@ def buscar_noticias():
 def home():
     noticias = buscar_noticias()
     return render_template('index.html', noticias=noticias)
-
-if __name__ == '__main__':
-    app.run(debug=True)
